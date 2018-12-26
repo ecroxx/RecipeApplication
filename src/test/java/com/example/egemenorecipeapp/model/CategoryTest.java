85cb614e99744662880d4c6a669fc0ef785aed64
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-27 02:36:55 +0300

    Some Unit Tests are added for Category class (CategoryTest)

---
+++ src/test/java/com/example/egemenorecipeapp/model/CategoryTest.java
@@ -0,0 +1,36 @@
+package com.example.egemenorecipeapp.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category= new Category();
+    }
+
+    @Test
+    public void getId() {
+        Long id= 4l;
+        category.setId(id);
+    assertEquals(id,category.getId());
+
+    }
+
+    @Test
+    public void getDescription() {
+        category.setDescription("english");
+        assertEquals("english",category.getDescription());
+    }
+
+    @Test
+    public void getRecipes() {
+       // Set<Recipe> recipeSet=new HashSet<>(Arrays.asList(new Recipe()));
+       //   category.setRecipes(recipeSet);
+    }
+}
