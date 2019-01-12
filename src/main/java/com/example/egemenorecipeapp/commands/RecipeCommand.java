0ddc67076f79acde3b51ca524f10f4f5cfb2f4c5
egemenozkan <egemen.ozkan@yahoo.com>
2019-01-12 04:17:29 +0300

    Validation constraints JSR303/Hibernate are added. ValidTest and some fix(for saveOrUpdate method) are addded.

--- src/main/java/com/example/egemenorecipeapp/commands/RecipeCommand.java
+++ src/main/java/com/example/egemenorecipeapp/commands/RecipeCommand.java
@@ -4,28 +4,50 @@ import com.example.egemenorecipeapp.model.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by jt on 6/21/17.
- */
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class RecipeCommand {
-    private Long id;
-    private Byte[] image;
-    private String description;
-    private Integer prepTime;
-    private Integer cookTime;
-    private Integer servings;
-    private String source;
-    private String url;
-    private String directions;
-    private Set<IngredientCommand> ingredients = new HashSet<>();
-    private Difficulty difficulty;
-    private NotesCommand notes;
-    private Set<CategoryCommand> categories = new HashSet<>();
+
+        private Long id;
+
+        @NotBlank
+        @Size(min = 3, max = 255)
+        private String description;
+
+        @Min(1)
+        @Max(999)
+        private Integer prepTime;
+
+        @Min(1)
+        @Max(999)
+        private Integer cookTime;
+
+        @Min(1)
+        @Max(100)
+        private Integer servings;
+        private String source;
+
+        @URL
+        private String url;
+
+        @NotBlank
+        private String directions;
+
+        private Set<IngredientCommand> ingredients = new HashSet<>();
+        private Byte[] image;
+        private Difficulty difficulty;
+        private NotesCommand notes;
+        private Set<CategoryCommand> categories = new HashSet<>();
+
 }
--- src/main/java/com/example/egemenorecipeapp/controllers/RecipeController.java
+++ src/main/java/com/example/egemenorecipeapp/controllers/RecipeController.java
@@ -7,14 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -53,9 +57,20 @@ public class RecipeController {
 
 
     @PostMapping("/recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
-        RecipeCommand savedCommand=recipeService.saveRecipeCommand(recipeCommand);
-        return "redirect:/recipe/"+savedCommand.getId()+"/show";
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
